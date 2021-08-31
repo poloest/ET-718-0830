074acee3116b847703bb4b4330484be780d40a8f
poloest <poloestrella007@gmail.com>
2021-08-31 14:23:54 -0400

    3 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -16,3 +16,4 @@ export class ProductAlertsComponent implements OnInit {
 
   ngOnInit() {}
 }
+
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -25,3 +25,4 @@ Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at https://angular.io/license
 */
+
